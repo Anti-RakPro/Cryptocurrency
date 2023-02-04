2ac8948864847dd8a15494aeeb4cade7e2147686
kvit.d.o@gmail.com <kvit.d.o@gmail.com>
2023-02-05 01:26:28 +0300

    init: add staff

--- cryptocurrency/src/App.jsx
+++ cryptocurrency/src/App.jsx
@@ -5,16 +5,18 @@ import Coins from "./components/Coins";
 import Navbar from "./components/Navbar";
 import Coin from "./routes/Coin";
 
+
+
 function App() {
   const [coins, setCoins] = useState([])
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false'
 
 
   useEffect(()=> {
     axios.get(url).then((response)=>{
       setCoins(response.data)
-      console.log(response)
-      console.log(response.data)
+      // console.log(response)
+      // console.log(response.data)
       // console.log(response.data[0])
     }).catch((error)=>{
       console.log(error)
